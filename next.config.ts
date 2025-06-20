68dc02499b6d66e71248e9e8f75188efe804bf17
Juan Murga <juanmurga97@gmail.com>
2025-06-20 11:41:33 +0200

    add images remotePatterns nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+    ],
+  },
+  experimental: {
+    viewTransition: true,
+  },
 };
 
 export default nextConfig;
