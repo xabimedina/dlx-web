fa32d79911b42b28deccc57a8b257f674ae49943
Juan Murga <juanmurga97@gmail.com>
2025-06-20 11:41:19 +0200

    add view transitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
-import type { Metadata } from "next"
-import { montserrat, kanit } from "@/assets/fonts";
-import "../assets/styles/globals.css";
-import "dlx-components/style.css";
-
-
+import type { Metadata } from 'next';
+import { montserrat, kanit } from '@/assets/fonts';
+import '../assets/styles/globals.css';
+import 'dlx-components/style.css';
+import { unstable_ViewTransition as ViewTransition } from 'react';
 
 export const metadata: Metadata = {
-  title: "DLX",
-  description: "DLX",
+  title: 'DLX',
+  description: 'DLX',
 };
 
 export default function RootLayout({
@@ -16,9 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang='es'>
       <body className={`${montserrat.variable} ${kanit.variable} antialiased `}>
-        {children}
+        <ViewTransition name='page'>{children}</ViewTransition>
       </body>
     </html>
   );
